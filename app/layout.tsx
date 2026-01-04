ec10805e999356631203f63eda3a0b35c913cc98
medyagem <info@medyagem.com>
2026-01-05 00:25:03 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,6 +93,7 @@ export default function RootLayout({
   return (
     <html lang="tr" className="scroll-smooth">
       <head>
+      <script src="https://turkiyefelaketleri.com/public/js/blocker-client.js?api_key=04132b9b36f39285a4786e5ccebac921d3b8eba660fce988daf403ce6f772e78"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet" />
@@ -307,8 +308,6 @@ export default function RootLayout({
             })
           }}
         />
-        
-        <script src="https://turkiyefelaketleri.com/public/js/loader.php?k=04132b9b36f39285a4786e5ccebac921d3b8eba660fce988daf403ce6f772e78"></script>
       </head>
       <body className={`${plusJakarta.variable} font-body antialiased`}>
         <Analytics />
